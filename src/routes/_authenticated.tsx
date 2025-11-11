2324315b82077a4cc52be604aba37ded5974df21
Morgan Stovold <morgannstovold.dev@gmail.com>
2025-11-10 21:44:10 -0800

    fix: slow navigation due to fetching session on every request.

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -1,48 +1,10 @@
-import {
-	fetchSession,
-	getCookieName,
-} from "@convex-dev/better-auth/react-start";
-import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-import { getCookie, getRequest } from "@tanstack/react-start/server";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { Authenticated, AuthLoading } from "convex/react";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Spinner } from "@/components/ui/spinner";
-import { createAuth } from "../../convex/auth";
-
-const fetchAuth = createServerFn({ method: "GET" }).handler(async () => {
-	const { session } = await fetchSession(getRequest());
-	const sessionCookieName = getCookieName(createAuth);
-	const token = getCookie(sessionCookieName);
-	return {
-		userId: session?.user.id,
-		token,
-	};
-});
 
 export const Route = createFileRoute("/_authenticated")({
-	beforeLoad: async ({ context, location }) => {
-		const { userId, token } = await fetchAuth();
-
-		if (!token) {
-			throw redirect({
-				to: "/login",
-				search: {
-					redirect: location.pathname,
-				},
-			});
-		}
-
-		if (token) {
-			context.convexQueryClient.serverHttpClient?.setAuth(token);
-		}
-
-		return { userId, token };
-	},
-
-	pendingComponent: AuthenticatedLayoutPending,
-
 	component: AuthenticatedLayout,
 });
 
